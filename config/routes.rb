c5e1730e6fe866a12ef8845ab10d16503f9ab6c5
TellmeforDWC <rikugame1003@gmail.com>
2021-10-03 12:45:03 +0000

    [finish]bookers1のdebug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
+
+  get "/", to: "homes#top", as:"top"
+
+  root to: "homes#top"
+
 end
